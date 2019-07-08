d46e745c3d49b324f0ab8349fa10a1dc4f862331
Carol <carolina.oliveira@zup.com.br>
2019-07-08 10:49:54 -0300

    Iniciar projeto

---
+++ src/main/java/com/br/zup/battle/system/BattleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.battle.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleSystemApplication.class, args);
+	}
+
+}
